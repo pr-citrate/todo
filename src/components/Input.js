fc0fc7253fad64ed00fd0d8e1f1ea4168d63b641
pr-citrate <pr.citrate@gmail.com>
2023-03-16 09:48:58 +0900

    update Input.js and ListItem.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,21 @@
 import React from 'react';
 function Input({ todos, setTodos }) {
   const [text, setText] = React.useState('');
+  const addTodo = () => setTodos([...todos, text]);
+  const handleOnChange = e => {
+    setText(e.target.value);
+  };
+  const handleOnKeypress = e => {
+    if (e.key === 'Enter') {
+      addTodo();
+    }
+  };
+
   return (
     <div className='Input'>
       <div className='input-container'>
-        <input type='text' placeholder='What needs to be done?' onChange={e => setText([e])} />
-        <button onClick={() => setTodos([...todos, text])}>Add</button>
+        <input type='text' placeholder='What needs to be done?' onChange={handleOnChange} onKeyPress={handleOnKeypress} />
+        <button onClick={addTodo}>Add</button>
       </div>
     </div>
   );
